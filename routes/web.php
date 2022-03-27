8ae786e8bc5ea0551c7437e88ff2139aa9b1d5e6
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-03-27 22:14:47 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', static function () {
+    return view('home.home');
+});
 
 Route::get('about', static function () {
     return view('home.about');
